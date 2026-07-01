aacf9f54e6bb62d400a4298f6cee3421fff7e8fa
Mihkal Jouste <mihkal.jouste@gmail.com>
2026-07-02 01:52:31 +0300

    Print error on failed parse

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-import Puzzle from './components/Puzzle.vue';
-import PuzzleMaker from './components/PuzzleMaker.vue';
+import Puzzle from './components/Puzzle.vue'
+import PuzzleMaker from './components/PuzzleMaker.vue'
 
 const puzzleParam = window.location.search.slice(8)
 
@@ -11,42 +11,22 @@ function decodeAndValidate(puzzleParam: string) {
         const puzzle = JSON.parse(atob(puzzleParam))
 
         // validation here
-     
-        console.log(puzzle)
+
         return puzzle
     } catch (e) {
-        window.alert(e);
+        window.alert("Linkin tulkitseminen epäonnistui!")
+        console.error(e)
     }
 }
-
-console.log(puzzleParam)
 
 const puzzle = puzzleParam ? decodeAndValidate(puzzleParam) : null
 const currentView = ref<'puzzle' | 'puzzle-maker'>(puzzle ? 'puzzle' : 'puzzle-maker')
 
-// const puzzle = [
-//     {
-//         title: 'Elukat',
-//         cards: ['Ankka', 'Lehmä', 'Valas', 'Jänes'],
-//     },
-//     {
-//         title: 'Juomat',
-//         cards: ['Vesi', 'Maito', 'Kalja', 'Kefiiri'],
-//     },
-//     {
-//         title: 'Ulokkeet',
-//         cards: ['Sarvi', 'Niemi', 'Jorma', 'Tatti'],
-//     },
-//     {
-//         title: 'Söpöt',
-//         cards: ['Miu', 'Mau', 'Jonna', 'Nasti'],
-//     },
-// ]
 </script>
 
 <template>
-  <Puzzle v-if="currentView === 'puzzle'" :puzzle />
-  <PuzzleMaker v-if="currentView === 'puzzle-maker'" />
+    <Puzzle v-if="currentView === 'puzzle'" :puzzle />
+    <PuzzleMaker v-if="currentView === 'puzzle-maker'" />
 </template>
 
 <style>
@@ -55,5 +35,4 @@ const currentView = ref<'puzzle' | 'puzzle-maker'>(puzzle ? 'puzzle' : 'puzzle-m
 * {
     box-sizing: border-box;
 }
-
 </style>
