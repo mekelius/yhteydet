67b3224d81fa59a0b67a8d8f425094e3c5a7402e
Mihkal Jouste <mihkal.jouste@gmail.com>
2026-07-03 01:33:46 +0300

    Fix the clicking business

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,16 +5,19 @@ const emit = defineEmits(['selectCard'])
 const { card, disabled } = defineProps(['card', 'disabled'])
 const cardText = ref(card.cardText)
 
+// Try to distinguish non-mouse clicks (such as from screenreaders) with the detail property
+// This is done to provide the touch experience where multiple cards can be tapped at once
+function onClick(e: UIEvent) {
+    if (e.detail === 0) emit('selectCard')
+}
 </script>
 
 <template>
     <div class="wrapper">
-        <button @click="emit('selectCard')"
-            :class="[
-                card.selected ? 'selected' : '', 
-                card.solved ? `color${card.color + 1} solved` : ''
-            ]"
-            :disabled="card.solved || disabled">
+        <button @touchstart.prevent="emit('selectCard')" @mousedown.left="emit('selectCard')" @click="onClick" :class="[
+            card.selected ? 'selected' : '',
+            card.solved ? `color${card.color + 1} solved` : ''
+        ]" :disabled="card.solved || disabled">
             {{ cardText }}
         </button>
     </div>
@@ -35,7 +38,7 @@ button {
     border: none;
 
     transition-property: background-color;
-    transition-duration: 0.1s;
+    transition-duration: 0.2s;
 
     @media screen and (max-width: 720px) {
         font-size: 16px;
